7be5b103d7d54329515211445be1425e4432529a
JJoriping <op@jjo.kr>
2025-07-13 00:14:34 +0900

    Support FormData

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,26 @@
-import type { NextEndpointTable, NextPageTable } from "./index.js";
+import type { NextEndpointTable, NextPageTable, TypedFormData } from "./index.js";
 
 export type DefaultRequestObject = {
   'query': never,
   'body': never
 };
-export type CallAPIOptions = Omit<RequestInit, 'method'|'body'>&{
-  'host'?: string
+export type CallAPIOptions = Omit<RequestInit, 'method'|'headers'|'body'>&{
+  'host'?: string,
+  'headers'?: Record<string, string>
 };
 export type CallAPIArgumentsOf<T extends keyof NextEndpointTable> = unknown extends NextEndpointTable[T]['req']
   ? [requestObject?:{ 'options': CallAPIOptions }]
   : [
     requestObject:{
       [key in keyof NextEndpointTable[T]['req'] as unknown extends NextEndpointTable[T]['req'][key] ? never : key]: key extends "query"
-          ? QueryObjectOf<NextEndpointTable[T]['req'][key]>
-          : NextEndpointTable[T]['req'][key]
+        ? QueryObjectOf<NextEndpointTable[T]['req'][key]>
+        : NextEndpointTable[T]['req'][key] extends TypedFormData<any>
+        ? FormData
+        : NextEndpointTable[T]['req'][key]
     }&{ 'options'?: CallAPIOptions }
   ]
 ;
-type QueryObjectOf<T extends string> = {
+export type QueryObjectOf<T extends string> = {
   [key in T as key extends `${string}?`|`${string}[]` ? never : key]: string
 }&{
   [key in T as key extends `${infer R}[]` ? R : never]: string[]
